d496a91e9e239fff90b3198428e062363899fa8c
csm <csm009177@gmail.com>
2023-11-06 12:51:07 +0900

    prac2 is about apply callback

---
+++ prac2.js
@@ -0,0 +1,12 @@
+function myCallback(result) {
+  console.log('Callback 함수가 호출되었습니다. 결과는 ' + result + ' 입니다.');
+}
+
+function doAsyncTask(callback) {
+  setTimeout(function () {
+    const result = "비동기 작업 완료!";
+    callback(result);
+  }, 100); // 2초 후에 callback 함수를 호출
+}
+
+doAsyncTask(myCallback); // myCallback 함수를 doAsyncTask 함수의 인자로 전달
